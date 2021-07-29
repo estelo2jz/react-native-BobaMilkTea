627b3c59d18491f9e3a4bc9f795a53fcf39f6a6b
esteloabell <manitobaaa@gmail.com>
2021-07-29 10:42:21 -0500

    Update Rewards.js

--- screens/Rewards.js
+++ screens/Rewards.js
@@ -2,13 +2,31 @@ import React from 'react';
 import {
   View,
   Text,
+  FlatList,
+  ImageBackground,
   StyleSheet
 } from 'react-native';
 
-const Rewards = () => {
+import { HeaderBar, CustomButton } from '../components';
+import { dummyData, COLORS, FONTS, SIZES, icons } from '../constants';
+
+import { connect } from 'react-redux'; 
+
+const Rewards = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <Text>Rewards</Text>
+      {/* Header */}
+      <HeaderBar />
+
+      {/* Details */}
+      <FlatList 
+        style={{
+          marginTop: -25,
+          borderTopLeftRadius: SIZES.radius * 2,
+          borderTopRightRadius: SIZES.radius * 2,
+          backgroundColor: COLORS.lightGray2
+        }}
+      />
     </View>
   )
 }
@@ -16,8 +34,6 @@ const Rewards = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
   }
 })
 
