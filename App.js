01c0bb90ff54cd661dbe71c845d473f5f1a00a4d
esteloabell <manitobaaa@gmail.com>
2021-07-29 10:43:23 -0500

    Update App.js

--- App.js
+++ App.js
@@ -4,18 +4,16 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native';
 import SplashScreen from 'react-native-splash-screen'
 
-
 import Tabs from "./navigation/tabs";
 
 const Stack = createStackNavigator();
 
-
 const App = () => {
-
+  
   React.useEffect(() => {
     // SplashScreen.hide();
   }, [])
-
+  
   return (
     <NavigationContainer>
       <Stack.Navigator
