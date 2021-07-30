8bc6d5de47b70073ad5c38d48763a3daf04d6903
esteloabell <manitobaaa@gmail.com>
2021-07-30 14:06:50 -0500

    Update Rewards.js

--- screens/Rewards.js
+++ screens/Rewards.js
@@ -13,6 +13,125 @@ import { dummyData, COLORS, FONTS, SIZES, icons } from '../constants';
 import { connect } from 'react-redux'; 
 
 const Rewards = ({navigation}) => {
+
+  function renderRewardPointSection() {
+    return (
+      <View
+        style={{
+          alignItems: 'center',
+          marginVertical: SIZES.padding,
+        }}
+      >
+        {/* Text */}
+        <Text
+          style={{
+            color: COLORS.primary,
+            ...FONTS.h1,
+            fontSize: 35,
+          }}
+        >
+          Rewards
+        </Text>
+
+        <Text
+          style={{
+            marginTop: 10,
+            color: COLORS.white,
+            width: SIZES.width * 0.6,
+            textAlign: 'center',
+            ...FONTS.h3,
+            lineHeight: 18
+          }}
+        >
+          You are 60 points away from your next reward.
+        </Text>
+
+        {/* Image */}
+        <ImageBackground
+          source={icons.reward_cup}
+          resizeMode="contain"
+          style={{
+            marginTop: SIZES.padding,
+            width: SIZES.width * 0.8,
+            height: SIZES.width * 0.8,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
+          <View
+            style={{
+              width: 70,
+              height: 70,
+              borderRadius: 35,
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: COLORS.white,
+            }}
+          >
+            <Text style={{ ...FONTS.h1 }}>280</Text>
+          </View>
+        </ImageBackground>
+      </View>
+    )
+  }
+
+  function renderButton() {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        {/* Scan */}
+        <CustomButton 
+          isPrimaryButton={true}
+          label="Scan in store"
+          containerStyle={{
+            width: 130,
+            paddingVertical: 5,
+            marginRight: SIZES.radius,
+            borderRadius: SIZES.radius * 2,
+          }}
+          labelStyle={{
+            ...FONTS.h3
+          }}
+          onPress={() => navigation.navigate('Location')}
+        />
+
+        {/* Redeem */}
+        <CustomButton 
+          isSecondaryButton={true}
+          label="Redeem"
+          containerStyle={{
+            width: 130,
+            paddingVertical: 5,
+            borderRadius: SIZES.radius * 2,
+          }}
+          labelStyle={{
+            ...FONTS.h3
+          }}
+          onPress={() => navigation.navigate('Location')}
+        />
+      </View>
+    )
+  }
+
+  function renderAvailableRewardsHeader() {
+    return (
+      <View
+        style={{
+          marginTop: SIZES.padding,
+          marginBottom: SIZES.radius,
+          paddingHorizontal: SIZES.padding,
+        }}
+      >
+        <Text style={{ color: COLORS.white, ...FONTS.h2 }}>Available Rewards</Text>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
       {/* Header */}
@@ -24,8 +143,48 @@ const Rewards = ({navigation}) => {
           marginTop: -25,
           borderTopLeftRadius: SIZES.radius * 2,
           borderTopRightRadius: SIZES.radius * 2,
-          backgroundColor: COLORS.lightGray2
+          backgroundColor: COLORS.lightGray,
         }}
+        data={dummyData.availableRewards}
+        keyExtractor={item => `${item.id}`}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={
+          <View>
+            {/* Rewards Point */}
+            {renderRewardPointSection()}
+
+            {/* Buttons */}
+            {renderButton()}
+
+            {/* Header Label */}
+            {renderAvailableRewardsHeader()}
+          </View>
+        }
+        renderItem={({item}) => {
+          return (
+            <View
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginHorizontal: SIZES.padding,
+                marginBottom: SIZES.base,
+                paddingVertical: SIZES.base,
+                borderRadius: 20,
+                backgroundColor: item.eligible ? COLORS.yellow : COLORS.gray2,
+              }}
+            >
+              <Text
+                style={{
+                  color: item.eligible ? COLORS.black : COLORS.lightGray2,
+                  ...FONTS.body3
+                }}
+              >{item.title}</Text>
+            </View>
+          )
+        }}
+        ListFooterComponent={
+          <View style={{marginBottom: 120}} />
+        }
       />
     </View>
   )
